cc944363097a881fcb6f548340bc6c4f3d4ecaa0
mohamedfathi68 <mohamedfathielqastawy@gmail.com>
2024-08-31 03:06:58 +0300

    first commit

---
+++ src/database/dbConnection.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+
+const connection = new Sequelize('mysql://urbkm9hkhea6gwn4:JUbcZHOFvGdiVd1aPIJp@bchnbnvvkkpybbzzsccs-mysql.services.clever-cloud.com:3306/bchnbnvvkkpybbzzsccs', {
+  host: "bchnbnvvkkpybbzzsccs-mysql.services.clever-cloud.com",
+  dialect: "mysql",
+});
+
+(async () => {
+  try {
+    await connection.authenticate();
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
+export default connection;
